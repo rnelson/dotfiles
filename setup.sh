937c6752083a0a14a39e942b5a50f4591c2924fd
Ross Nelson <ross.nelson@gmail.com>
2015-07-14 11:19:19 -0500

    Solarized colors for vim

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # Make necessary directories
 mkdir -p ~/.vim/bundle
@@ -8,6 +8,9 @@ mkdir -p ~/.vim/colors
 mkdir -p ~/.vim/bundle
 if [[ ! -d ~/.vim/bundle/Vundle.vim ]]; then
     git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+if [[ ! -d ~/.vim/bundle/vim-colors-solarized ]]; then
+    git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
 fi
 
 # Copy config files
